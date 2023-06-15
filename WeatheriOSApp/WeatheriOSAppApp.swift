7a172ae99df4a024e006c2ffcde7fef3e5be5d33
DanielSarkwa <dhannysark@outlook.com>
2023-06-15 19:32:15 +0300

    Initial Commit

---
+++ WeatheriOSApp/WeatheriOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheriOSAppApp.swift
+//  WeatheriOSApp
+//
+//  Created by Daniel Yeboah on 15.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatheriOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
